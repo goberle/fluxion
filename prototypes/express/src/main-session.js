942db4f21896df8e578ac7607a257a959e2a653d
Etienne Brodu <etn@etnbrd.com>
2014-02-05 15:10:24 +0100

    ajout de main-session.js : surcouche express pour abstraire les id d'utilisateur

---
+++ prototypes/express/src/main-session.js
@@ -0,0 +1,29 @@
+var express = require('express')();
+
+app.getAsSession("/", function() {
+    this.count = 0;
+
+    this.process() {
+      return this.count += 1;
+    }
+  }
+)
+
+var app = function() {
+  this.users = {};
+  this.getter;
+
+  this.getAsSession = function(route, fn) {
+    this.getter = fn;
+  }
+
+  this.send = function(id, route, res) {
+    res.send( (this.users[id] = this.users[id] || this.getter()).process() );
+  }
+}
+
+var users = {};
+
+express.get('/:id', function(req, res) {
+  app.send(req.params.id, res);
+})
