1fd2deb31d522e92ff76a944f93b51d35aff7344
Etienne Brodu <etn@etnbrd.com>
2014-02-05 15:47:43 +0100

    fix execution errors

--- prototypes/express/src/main-session.js
+++ prototypes/express/src/main-session.js
@@ -1,17 +1,6 @@
 var express = require('express')();
 
-app.getAsSession("/", function() {
-    this.count = 0;
-
-    this.process = function() {
-      return this.count += 1;
-    }
-
-    return this;
-  }
-)
-
-var app = function() {
+appFactory = function() {
   this.users = {};
   this.getter;
 
@@ -19,13 +8,32 @@ var app = function() {
     this.getter = fn;
   }
 
-  this.send = function(id, route, res) {
-    res.send( (this.users[id] = this.users[id] || this.getter()).process() );
+  this.send = function(id, res) {
+    res.send( '' + (this.users[id] = this.users[id] || this.getter()).calcul() );
   }
-}
+
+  return this;
+};
+
+var app = appFactory();
+
+app.getAsSession("/", function() {
+    this.count = 0;
+
+    this.calcul = function() {
+      return this.count += 1;
+    }
+
+    return this;
+  }
+)
 
 var users = {};
 
 express.get('/:id', function(req, res) {
   app.send(req.params.id, res);
 })
+
+port = 8080;
+express.listen(port);
+console.log("listening port: " + port);
