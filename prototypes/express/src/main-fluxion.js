a6dbbe850782b32112bf7b2c02efd2a5798cb083
Etienne Brodu <etn@etnbrd.com>
2014-02-10 18:49:04 +0100

    typo

--- prototypes/express/src/main-fluxion.js
+++ prototypes/express/src/main-fluxion.js
@@ -5,7 +5,6 @@ var wflx = require('./web-fluxions');
 wflx.register("/", ["uid"], {count: 0}, function(){
   this.count += 1;
   return m("send", this.count);
-  return undefined;
 });
 
 wflx.listen(8080);
